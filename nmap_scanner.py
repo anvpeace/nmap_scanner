4d92e0f61358ff35c9557229a98bfd28fccdd821
anvpeace <cruzpeacem@gmail.com>
2025-06-22 08:43:24 -0400

    final updates

--- nmap_scanner.py
+++ nmap_scanner.py
@@ -17,7 +17,7 @@ resp = input(""" \nPlease enter the type of scan you want to run
                    2) UDP Scan
                    3) Comprehensive Scan \n""")
 
-# print("You have selected option: ", resp)
+print("You have selected option: ", resp)
 
 if resp == '1':
     # first print 
@@ -37,3 +37,34 @@ if resp == '1':
 
     # display all open ports with keys method - keys returns all active ports
     print("Open ports: ", scanner[ip_addr]['tcp'].keys())
+
+elif resp == '2':
+    print("Nmap version: ", scanner.nmap_version())
+    
+    scanner.scan(ip_addr, '1-1024', '-v -sU')
+
+    print(scanner.scaninfo())
+
+    # tells user if the ip address is online or offline
+    print("IP Status: ", scanner[ip_addr].state())
+
+    # print all protocols
+    print(scanner[ip_addr].all_protocols())
+
+    # display all open ports with keys method - keys returns all active ports
+    print("Open ports: ", scanner[ip_addr]['udp'].keys())
+
+elif resp == '3':
+    print("Nmap version: ", scanner.nmap_version())
+
+    scanner.scan(ip_addr, '1-1024', '-v -sS -sV -sC -A -O')
+
+    print(scanner.scaninfo())
+
+    # .state() used to give status of ip address
+    print("IP Status: ", scanner[ip_addr].state)
+    print(scanner[ip_addr].all_protocols())
+    print("Open ports: ", scanner[ip_addr]['tcp'].keys())
+
+elif resp >= '4':
+    print("Please enter a valid option")
